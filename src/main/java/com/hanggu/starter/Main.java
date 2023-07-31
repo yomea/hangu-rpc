e0c0e73f8ceca78158de6c9a891112417fdd42c7
wuzhenhong <wuzhenhong@banksteel.com>
2023-07-31 10:28:40 +0800

    init

---
+++ src/main/java/com/hanggu/starter/Main.java
@@ -0,0 +1,15 @@
+package com.hanggu.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuzhenhong
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
