84277af1df48bc2dfb020a3d2fddf309cfcb5a04
wuzhenhong <wuzhenhong@banksteel.com>
2024-02-19 19:23:21 +0800

    fix：修复自定义转换器未生效的问题

--- hangu-rpc-spring-boot-starter/src/main/java/com/hangu/provider/configuration/ProviderConfiguration.java
+++ hangu-rpc-spring-boot-starter/src/main/java/com/hangu/provider/configuration/ProviderConfiguration.java
@@ -1,10 +1,17 @@
 package com.hangu.provider.configuration;
 
 import com.hangu.provider.listener.ProviderApplicationListener;
+import com.hangu.provider.resolver.MethodArgumentResolver;
 import com.hangu.provider.resolver.MethodArgumentResolverHandler;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.format.support.FormattingConversionService;
 
 /**
  * Created by wuzhenhong on 2023/8/1 23:53
@@ -18,7 +25,20 @@ public class ProviderConfiguration {
     }
 
     @Bean
-    public MethodArgumentResolverHandler methodArgumentResolverHandler() {
-        return new MethodArgumentResolverHandler();
+    public ConversionService customerConversionService() {
+        FormattingConversionService conversionService = new FormattingConversionService();
+//        conversionService.addConverter(new StringToCharConverter());
+        return conversionService;
+    }
+
+    @Bean
+    public MethodArgumentResolverHandler methodArgumentResolverHandler(@Autowired(required = false) ConversionService customerConversionService,
+        @Autowired(required = false) List<MethodArgumentResolver> resolverList) {
+        // conversionService 可自定义
+        MethodArgumentResolverHandler resolverHandler = new MethodArgumentResolverHandler(customerConversionService);
+//        如果当前满足不了你的参数解析需求，可以自己扩展，然后可以自己加排序
+        Optional.ofNullable(resolverList).orElse(Collections.emptyList())
+            .forEach(resolverHandler::addCustomerResolver);
+        return resolverHandler;
     }
 }
