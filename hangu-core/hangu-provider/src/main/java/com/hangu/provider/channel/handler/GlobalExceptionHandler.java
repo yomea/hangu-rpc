af603251b683b1648d84d59156778f3526c6c904
wuzhenhong <wuzhenhong@banksteel.com>
2024-02-22 11:28:52 +0800

    fix：移除全局异常Attribute信息

--- hangu-core/hangu-provider/src/main/java/com/hangu/provider/channel/handler/GlobalExceptionHandler.java
+++ hangu-core/hangu-provider/src/main/java/com/hangu/provider/channel/handler/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import com.hangu.common.util.CommonUtils;
 import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.Attribute;
 import io.netty.util.AttributeKey;
 import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +24,8 @@ public class GlobalExceptionHandler extends ChannelDuplexHandler  {
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         log.error("请求处理失败！", cause);
-        Request request = ctx.channel().attr(hanguCons.REQUEST_ATTRIBUTE_KEY).get();
+        Attribute<Request> requestAttribute = ctx.channel().attr(hanguCons.REQUEST_ATTRIBUTE_KEY);
+        Request request = requestAttribute.getAndSet(null);
         if(Objects.nonNull(request)) {
             // 如果是其他在处理请求过程中抛出的异常，那么需要通知到消费者
             Response response = CommonUtils.createResponseInfo(request.getId(), request.getSerializationType(),
